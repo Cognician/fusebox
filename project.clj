01671377d0fb6919dd03f1ad73c7b36021e5cec9
Robert Stuttaford <robert@cognician.com>
2013-08-29 16:44:47 +0200

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cognician/fusebox "0.1.1"
+(defproject cognician/fusebox "0.1.2"
   :description "Fusebox: a feature flag system for Clojure and ClojureScript."
   :url "https://github.com/cognician/fusebox"
   :license {:name "Eclipse Public License"
